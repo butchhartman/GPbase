1c9e28aa7dfa40e83512bbd2f026d7029a1ed46b
rcama <rcamacho5436@gmail.com>
2025-03-03 13:16:16 -0700

    Documented all functions properly. Cleaned up code and improved functionality of validation layer functions.
    Added #defines for debug verbosity.
    
    (Primarily getRequiredExtensions. It works now.)

--- include/GPbase.h
+++ include/GPbase.h
@@ -2,35 +2,47 @@
 // or project specific include files.
 
 #pragma once
-// Ensures GLFW won't include the development environment header. This prevents conflicts upon inclusion of gl3w .
 #define SDL_MAIN_USE_CALLBACKS 1 // uses callbacks instead of the main as an entry point
 
 #define DEFAULT_WIDTH 640
 #define DEFAULT_HEIGHT 480
 
 #ifndef DEBUG_MODE
-const unsigned int enableValidationLayers = 0;
+	const unsigned int enableValidationLayers = 0;
+
 #else
-const unsigned int enableValidationLayers = 1;
+	const unsigned int enableValidationLayers = 1;
+
+	#ifdef DEBUG_VERBOSITY_ALL // No restruction to debug mode outputs
+		#define DEBUG_VERBOSITY 0 
+	#endif\
+
+	#ifdef DEBUG_VERBOSITY_CRITICAL // Only critical debug mode outpus will be logged
+		#define DEBUG_VERBOSITY 256 //VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT
+	#endif 
+
+
 #endif
 
+// Validations layers
 const char* validationLayers[] = {
-	"VK_LAYER_KHRONOS_validation"
+	"VK_LAYER_KHRONOS_validation",
 };
 
+// Standard libraries
 #include <stdio.h>
 #include <string.h>
 
-// Window creation library. Also handles other things like input.
+// SDL : Window creation, audio, input, and 2D graphics library
 #include "SDL3/SDL.h"
 #include "SDL3/SDL_main.h"
 #include "SDL3/SDL_vulkan.h"
-// C compatible math library for OpenGL
+
+// Graphics math library
 #include "cglm/cglm.h"
+
+// 3D graphics API
 #include <vulkan/vulkan.h>
-// Custom defines
-// Global variables
-// Bad practice but useful for this application.
 
 
 
--- src/sdlMain.c
+++ src/sdlMain.c
@@ -1,15 +1,25 @@
-// Code provided by :
+// SDL Code provided by :
 // https://github.com/libsdl-org/SDL/blob/main/examples/renderer/01-clear/clear.c
+
+// Much of the Vulkan code was provided by or adapted from :
+// https://vulkan-tutorial.com/
+
 #define DEBUG_MODE
+#define DEBUG_VERBOSITY_ALL
+
 #include <GPbase.h>
 
 // renderer is used to draw into the window every frame.
 static SDL_Window* window = NULL;
 static SDL_Renderer* renderer = NULL;
+
 static VkInstance instance;
 VkDebugUtilsMessengerEXT debugMessenger;
 
-
+/*
+Gets the vkDestroyDebugUtilsMessengerEXT function
+extension and uses it to desroy / clean up the given instance's debug messenger
+*/
 void DestroyDebugUtilsMessengerEXT(VkInstance vinstance,
 	VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
 
@@ -23,6 +33,10 @@ void DestroyDebugUtilsMessengerEXT(VkInstance vinstance,
 
 }
 
+/*
+Loads the CreateDebugUtilMessengerEXT function extension and uses it to 
+create a debug messenger for the given instance
+*/
 VkResult CreateDebugUtilMessengerEXT(VkInstance vinstance,
 	const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator,
 	VkDebugUtilsMessengerEXT* pDebugMessenger) {
@@ -35,21 +49,27 @@ VkResult CreateDebugUtilMessengerEXT(VkInstance vinstance,
 	}
 }
 
-
+/*
+Debug callback. Is called and provided with pCallbackData when a validation layer message is sent.
+Decides to output the message if its verbosity level is greater than or equal to the requested debug 
+verbosity.
+*/
 static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
 	VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
 	VkDebugUtilsMessageTypeFlagsEXT messageType,
 	const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
 	void* pUserData) {
 
-
-	if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) {
+	if (messageSeverity >= DEBUG_VERBOSITY) {
 		printf("\nvalidation layer: %s", pCallbackData->pMessage);
 	}
 
 	return VK_FALSE;
 }
 
+/*
+When passed with a createInfo pointer, initializes the data instance that struct.
+*/
 void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT *createInfo) {
 	memset(createInfo, 0, sizeof(VkDebugUtilsMessengerCreateInfoEXT));
 	createInfo->sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
@@ -61,33 +81,48 @@ void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT *create
 	createInfo->pUserData = NULL;
 }
 
-void setupDebugMessenger() {
+/*
+If validation layers are enabled, initalizes a VkDebugUtilsMessengerCreateInfoEXT struct with
+populateDebugMessengerCreateInfo call, then creates a debug messenger with a CreateDebugUtilMessengerEXT
+*/
+void setupDebugMessenger(VkInstance vinstance) {
 	if (!enableValidationLayers) return;
 
 	VkDebugUtilsMessengerCreateInfoEXT createInfo = { 0 };
 	populateDebugMessengerCreateInfo(&createInfo);
 
-	if (CreateDebugUtilMessengerEXT(instance, &createInfo, NULL, &debugMessenger) != VK_SUCCESS) {
+	if (CreateDebugUtilMessengerEXT(vinstance, &createInfo, NULL, &debugMessenger) != VK_SUCCESS) {
 		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to set up debug messenger.");
 	}
 
 }
 
 
-
-const char* getRequiredExtensions(uint32_t *count) {
-	uint32_t SDL3ExtensionCount = 0;
-	const char** SDL3Extensions;
+/*
+Returns a list of extensions needed by Vulkan for the application.
+Assignes the passes count pointer to the number of extensions required.
+If validation layers are enabled, appends VK_EXT_DEBUG_UTILS_EXTENSION_NAME to the
+end of the returned array and adds 1 to the count.
+*/
+const char **getRequiredExtensions(uint32_t *count) {
+	const char **SDL3Extensions;
 
 	SDL3Extensions = SDL_Vulkan_GetInstanceExtensions(count);
-	if (enableValidationLayers) {
-		const char **extensions = (const char**)malloc(sizeof(SDL3Extensions) + sizeof(SDL3Extensions[0]));
-		memcpy(extensions, SDL3Extensions, sizeof(SDL3Extensions) + sizeof(SDL3Extensions[0]));
-		extensions[SDL3ExtensionCount+1] = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
 
-		for (uint32_t i = 0; i < SDL3ExtensionCount; i++) {
-			printf("\EXT: %s\n", extensions[SDL3ExtensionCount]);
+	if (enableValidationLayers) {
+		const char **extensions = malloc(sizeof(const char*) * (*(count)+1));
+
+
+		for (uint32_t i = 0; i < *count; i++) {
+			extensions[i] = SDL3Extensions[i];
 		}
+		extensions[*(count)] = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
+
+		for (uint32_t i = 0; i < *(count) + 1; i++) {
+			SDL_Log("REQUIRED EXTENSION: %s", extensions[i]);
+		}
+
+		*count += 1;
 
 		return extensions;
 	}
@@ -97,18 +132,33 @@ const char* getRequiredExtensions(uint32_t *count) {
 
 }
 
+/*
+	Checks the amount of available layers and sees if any of them
+	match the defined validation layers for the application.
+	TODO: This function is poorly designed and only supports having a single valiaiton layer requested. Fix.
+	Ignore all outputs from this function.
+*/
 uint32_t checkValidationLayerSupport() {
 	uint32_t layerCount;
 	vkEnumerateInstanceLayerProperties(&layerCount, NULL);
 
-	VkLayerProperties *availableLayers = (VkLayerProperties*)malloc(layerCount * sizeof(VkLayerProperties));
+
+	VkLayerProperties* availableLayers = (VkLayerProperties*)malloc(layerCount * sizeof(VkLayerProperties));
 	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers);
+
+	//SDL_Log("%d available validation layers.", layerCount);
+	//
+	//for (uint32_t i = 0; i < layerCount; i++) {
+	//	SDL_Log("\t%s", availableLayers[i]);
+	//}
 
 	uint32_t layerFound = 0;
 
 	for (uint32_t i = 0; i < layerCount; i++) {
+	//	SDL_Log("Finding validation layer %d...", i);
 
 		for (uint32_t j = 0; j < sizeof(validationLayers) / sizeof(validationLayers[0]); j++) {
+
 			if (strcmp(availableLayers[i].layerName, validationLayers[j]) == 0) {
 				layerFound = 1;
 				break;
@@ -116,19 +166,26 @@ uint32_t checkValidationLayerSupport() {
 
 		}
 
-
+		//if (layerFound) {
+		//	SDL_Log("Validation layer found");
+		//}
+		//else {
+		//	SDL_Log("Failed to find validation layer");
+		//}
 	}
+		if (!layerFound) {
+			return 0;
+		}
 
-	if (!layerFound) {
-		return 0;
-	}
-
-	return 1;
+		return 1;
+	
 }
 
 
-/* Function that creates the Vulkan instance. Must be called after
-loading Vulkan via SDL3.*/
+/* 
+	Function that creates the Vulkan instance. Must be called after
+	loading Vulkan via SDL3.
+*/
 VkResult Vk_Init() {
 	if (enableValidationLayers) {
 		SDL_Log("=========Debug mode=========");
@@ -190,7 +247,7 @@ VkResult Vk_Init() {
 		SDL_Log("Vulkan instance successfully created.");
 	}
 
-	setupDebugMessenger();
+	setupDebugMessenger(instance);
 
 	// Skipped implementing the VK_ERROR_INCOMPATIBLE_DRIVER workaround. Dont know if it's necessary, sorry mac.
 
@@ -214,7 +271,9 @@ VkResult Vk_Init() {
 	return VK_SUCCESS;
 }
 
-// This function runs once on startup
+/*
+	Initialization callback ran by SDL when the application starts.
+*/
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
 
 	SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
@@ -237,7 +296,9 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
 	return SDL_APP_CONTINUE; // The init function ran successfully
 }
 
-// This function runs when a new event (mouse input, keypress, etc) occurs.
+/*
+	Function that is called by SDL whenever an input event is proc'd
+*/
 SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
 
 	if (event->key.scancode == SDL_SCANCODE_ESCAPE) {
@@ -251,7 +312,9 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
 	return SDL_APP_CONTINUE; // Carry on with the program
 }
 
-// This function runs once per frame. It is the beating heart behind the program
+/*
+	Function ran once per frame by SDL. Similar to the Update() loop of game engines.
+*/
 SDL_AppResult SDL_AppIterate(void* appstate) {
 	const double now = ((double)SDL_GetTicks()) / 1000.0f; // converts from ms to s
 
@@ -270,14 +333,16 @@ SDL_AppResult SDL_AppIterate(void* appstate) {
 	return SDL_APP_CONTINUE; // Carry on with the program
 }
 
-// This function runs once when the program is closed.
+/* 
+	Function ran by SDL when the application closes. Stores cleanup procedures
+*/
 void SDL_AppQuit(void* appstate, SDL_AppResult result) {
 	// SDL cleans up the window/renderer for us
 	SDL_Log("Application terminating.");
 
 	if (enableValidationLayers) {
-	//	DestroyDebugUtilsMessengerEXT(instance, debugMessenger, NULL);
+		DestroyDebugUtilsMessengerEXT(instance, debugMessenger, NULL);
 	}
 
-	//vkDestroyInstance(instance, NULL);
+	vkDestroyInstance(instance, NULL);
 }
